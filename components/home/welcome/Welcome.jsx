9f1425c4119a7b1eeb2138fc2393783c903cd9c6
Yassine Azzimani <yassineazzimani@hotmail.fr>
2023-10-28 12:36:58 +0200

    Update README.md The app is ready to be implement

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome to CinéSuggest</Text>
+    </View>
+  )
+}
+
+export default Welcome
---
+++ components/index.js
@@ -0,0 +1,29 @@
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+
+// home screen
+import Welcome from "./home/welcome/Welcome";
+import Nearbyjobs from "./home/nearby/NearbyMovies";
+import Popularjobs from "./home/popular/Popularmovies";
+
+// job details screen
+import Company from "./movies_details/company/Company";
+import { default as JobTabs } from "./movies_details/tabs/Tabs";
+import { default as JobAbout } from "./movies_details/about/About";
+import { default as JobFooter } from "./movies_details/footer/Footer";
+import Specifics from "./movies_details/specifics/Specifics";
+
+// common
+import NearbyJobCard from "./common/cards/nearby/NearbyMovieCard";
+
+export {
+  ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard
+};
