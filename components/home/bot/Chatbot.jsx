50e8e731c340aaa7fd7c60102400689e621ce939
Yassine Azzimani <yassineazzimani@hotmail.fr>
2023-10-28 14:04:55 +0200

    Merge pull request #7 from YassineAzz/add_page_and_styles First div already ok

---
+++ components/home/bot/Chatbot.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './Chatbot.style'
+
+const Chatbot = () => {
+  return (
+    <View>
+      <Text>Chatbot CineSuggest</Text>
+    </View>
+  )
+}
+
+export default Chatbot
