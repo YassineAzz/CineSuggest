3707d97fe5be1332293d1ac324f666bf07fe767d
Yassine Azzimani <yassineazzimani@hotmail.fr>
2023-10-29 21:25:17 +0100

    Update README.md

--- components/home/bot/Chatbot.jsx
+++ components/home/bot/Chatbot.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import { useState} from 'react'
 import { View, Text } from 'react-native'
-
+import { COLORS } from '../../../constants';
 import styles from './Chatbot.style'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const sendMessage = () => {
+    if (message) {
+      setMessages([...messages, message]);
+      setMessage('');
+    }
+  };
+
   return (
-    <View>
-      <Text>Chatbot CineSuggest</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1,  backgroundColor: COLORS.lightWhite}}>
+      <SafeAreaView style={styles.container}>
+      <div style={styles.headerTitle} className="chat">
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <input
+        style={styles.headerTitle}
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={ (e) => {
+          if (e.key === 'Enter') {
+            sendMessage()
+          }
+        }}
+        placeholder="Mot clé de vos envies"
+      />
+      </SafeAreaView>
+      <button style={styles.headerTitle} onClick={sendMessage}>Valider</button>
+    </SafeAreaView>
+  );
 }
 
 export default Chatbot
